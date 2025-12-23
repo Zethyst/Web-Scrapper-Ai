c5c4ebd01aa5c22bc6407d74b788fa3472f1a160
Ekansh <zethyst@protonmail.com>
2025-12-24 02:33:22 +0530

    Added Favicon and Skeletons

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    theme: {
+      extend: {
+        animation: {
+          'shimmer': 'shimmer 2s infinite',
+          'shimmer-fast': 'shimmer 1.5s infinite',
+        },
+        keyframes: {
+          shimmer: {
+            '0%': { transform: 'translateX(-100%)' },
+            '100%': { transform: 'translateX(100%)' },
+          },
+        },
+      },
+    },
+  }
